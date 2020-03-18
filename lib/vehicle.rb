de16d9ac1956a7aa399482590d43ecb7e127bbcd
Karim Macknojia <alishahal@msn.com>
2020-03-18 04:50:57 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -6,7 +6,20 @@ attr_accessor :wheel_size, :wheel_number
   end
  
   def go
+    attr_accessor :wheel_size, :wheel_number
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+ 
+  def go
     "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+ 
+  def fill_up_tank
+    "filling up!"
+  end
+ 
   end
  
   def fill_up_tank
